7341d76cccf0b262167dd8f3d97ce5e521e594f0
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-27 16:16:34 +0200

    adding transition effect to welcom page and set interval

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const btnOpenModal = document.querySelectorAll(".btn--show-modal");
+const loaderTexts = document.querySelectorAll(".loader--text");
+const main = document.querySelector(".main");
+const header = document.querySelector(".header");
+///////////////////////////////////////
+// Loader Text Animation
+
+///////////////////////////////////////////////////////////
+loaderTexts.forEach((t) => {
+  window.addEventListener("load", (e) => {
+    t.classList.add("target");
+  });
+});
+
+setInterval(function () {
+  main.classList.add("hidden");
+  header.classList.remove("hidden");
+}, 1500);
+
 /* TITLE ANIMATION */
 let didScroll = false;
 let paralaxTitles = document.querySelectorAll(".paralax-title");
@@ -9,6 +31,7 @@ const scrollInProgress = () => {
 const raf = () => {
   if (didScroll) {
     paralaxTitles.forEach((element, index) => {
+      console.log(window.scrollY);
       element.style.transform = "translateX(" + window.scrollY / 10 + "%)";
     });
     didScroll = false;
