cfc9bce891623390ec6099cdd8fa6577f60f5b7d
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-27 23:03:05 +0200

    add circle pointer mouse

--- js/main.js
+++ js/main.js
@@ -6,10 +6,40 @@ const loaderTexts = document.querySelectorAll(".loader--text");
 const main = document.querySelector(".main");
 const header = document.querySelector(".header");
 const imgLoading = document.querySelector(".loader--img");
+
+///////////////////////////////////////
+// Cursor circle
+
+document.addEventListener("DOMContentLoaded", () => {
+  let mousePosX = 0,
+    mousePosY = 0,
+    mouseCircle = document.getElementById("mouse-circle");
+
+  document.onmousemove = (e) => {
+    mousePosX = e.pageX;
+    mousePosY = e.pageY;
+  };
+
+  let delay = 6,
+    revisedMousePosX = 0,
+    revisedMousePosY = 0;
+
+  function delayMouseFollow() {
+    requestAnimationFrame(delayMouseFollow);
+
+    revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+    revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+    mouseCircle.style.top = revisedMousePosY + "px";
+    mouseCircle.style.left = revisedMousePosX + "px";
+  }
+
+  delayMouseFollow();
+});
+
 ///////////////////////////////////////
 // Loader Text Animation
 
-///////////////////////////////////////////////////////////
 loaderTexts.forEach((t) => {
   window.addEventListener("load", (e) => {
     t.classList.add("target");
